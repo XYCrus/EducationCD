8f43df721e6faab463376ea7ce08242a61409243
XYCrus <q123902740@gmail.com>
2022-12-04 01:06:37 -0500

    fixed

--- source/pipeline_activation.py
+++ source/pipeline_activation.py
@@ -5,9 +5,13 @@ import csv
 
 if __name__ == '__main__':
     iteration = sys.argv[1]
+    dataset_option = sys.argv[2]
     
     for walk in np.arange(1, int(iteration) + 1):
-        os.system('cmd /c "python leave_n_split.py ../data/wholedata.csv {}"'.format(walk))
+        if dataset_option == '1':
+            os.system('cmd /c "python leave_n_split.py ../data/wholedata.csv {}"'.format(walk))
+        elif dataset_option == '2': 
+            os.system('cmd /c "python leave_n_split.py ../data/data2.csv {}"'.format(walk))
         os.system('cmd /c "python statistic_training_complete.py ../model/training_dataset.csv ../result ../model"')
         os.system('cmd /c "python statistic_prediction.py ../model/testing_dataset.csv ../result ../model"')
         os.system('cmd /c "python statistic_performance_metrics.py ../result {}"'.format(walk))
